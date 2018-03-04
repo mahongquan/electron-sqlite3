edebcab566ee69ae9a8578f54bf8a1004adea8b4
mahongquan <mahongquan@sina.com>
2018-03-04 08:44:07 +0800

    init

---
+++ db.js
@@ -0,0 +1,27 @@
+var addon = require('./hello_nodegyp/index')
+for(var i in addon)
+{
+	console.log(i);
+}
+console.log(addon.add(3,4));
+console.log(addon.path)
+///////////////////////////////////////
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+      var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (var i = 0; i < 10; i++) {
+                stmt.run("Ipsum " + i);
+                  }
+          stmt.finalize();
+
+            //db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+                    //console.log(row.id + ": " + row.info);
+                      //});
+});
+
+//db.close();
+
